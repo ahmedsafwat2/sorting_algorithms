ba81723f2b26d3a2e8a00b16c5e19995e886ec25
asosnses <asosnses@gmail.com>
2023-07-25 18:52:34 -0700

    insertion 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,6 +8,8 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j, status = 0;
 
+	if (array == NULL)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
