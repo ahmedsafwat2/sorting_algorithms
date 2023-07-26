ae2fa0fe778f7e71fa88e433645e27c89e09693b
asosnses <asosnses@gmail.com>
2023-07-25 18:21:32 -0700

    bubble 1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j, status = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				status |= swap_num(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+		if (status == 0)
+			break;
+		status = 0;
+	}
+}
+
+unsigned int swap_num(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+	return 1;
+}
